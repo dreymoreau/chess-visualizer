6318c42d278930f4b76d7e813457fbf4a883e69a
dreymotoh <striclyspoken@gmail.com>
2023-10-21 14:55:59 -0400

    profile object showing!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
-export default function App() {
+import { useEffect, useState } from 'react';
+import ChessWebAPI, { PlayerProfileResponse } from "chess-web-api"
+
+function ChessPlayerProfile() {
+  const [playerProfile, setPlayerProfile] = useState<PlayerProfileResponse | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const chessAPI = new ChessWebAPI();
+
+    chessAPI
+      .getPlayer('skyspace8888')
+      .then(
+        (response) => {
+          setPlayerProfile(response.body);
+        },
+        (err) => {
+          setError(err);
+        }
+      );
+  }, []);
 
   return (
-    <></>
-  )
+    <div>
+      <h2>Player Profile</h2>
+      {error ? (
+        <p>Error: {error.message}</p>
+      ) : playerProfile ? (
+        <pre>{JSON.stringify(playerProfile, null, 2)}</pre>
+      ) : (
+        <p>Loading player profile...</p>
+      )}
+    </div>
+  );
 }
+
+export default ChessPlayerProfile;
