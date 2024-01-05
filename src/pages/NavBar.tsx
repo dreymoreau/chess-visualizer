49c1d02cf097bf29dd991bc908b883ade5d99b79
dreymotoh <striclyspoken@gmail.com>
2024-01-05 12:49:58 -0500

    starting to implement hamburger for responsiveness

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -8,8 +8,8 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ links }) => {
   return (
     <header>
-    <nav className="bg-[#34d399] flex flex-col justify-between px-4 py-6 shadow-xl">
-      <ul className="flex">
+    <nav className="bg-[#34d399] flex flex-col justify-between px-4 py-6 shadow-xl" id="hamburger-menu">
+      <ul className="flex" id="sidebar-menu">
         {links.map((link, index) => (
           <li key={index} className="mr-4">
             <Link to={link.path}>{link.label}</Link>
